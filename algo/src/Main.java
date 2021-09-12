a5e9b6928eaf2de8e108277e888d0689a3ba88d0
HimmelSpark <adam404pet@gmail.com>
2021-09-12 12:23:20 +0300

    isPrime for O(√N)

--- algo/src/Main.java
+++ algo/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         fibo(10);  // 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
+        isPrime(367, 168);
     }
 
     private static void fibo(int N) {
@@ -10,5 +11,11 @@ public class Main {
         }
         Fibo fibo = new Fibo(N);
         System.out.printf("Optimized fibo for N = %d: %d", N, fibo.getFiboRecOptimized(N));
+    }
+
+    private static void isPrime(int prime, int notPrime) {
+        System.out.printf("%d is prime -> %b", prime, Prime.isPrime(prime));
+        System.out.println();
+        System.out.printf("%d is prime -> %b", notPrime, Prime.isPrime(notPrime));
     }
 }
